3f19622047e6b8e1d1c551625bb44a4467c61413
Mauro Silberberg <maurosilber@gmail.com>
2019-12-30 13:49:49 -0300

    Improved read_header_pt3 and added tests
    Added context manager to close file descriptor.
    
    Signed-off-by: Mauro Silberberg <maurosilber@gmail.com>

--- pyflim/io/pq_header.py
+++ pyflim/io/pq_header.py
@@ -30,8 +30,10 @@ def read_header_ptu(path):
     
     Returns
     -------
-    header, records_start : dict, int
-        The header of the ptu file and file location where the TTTR information starts.
+    header : dict
+        The header of the ptu file.
+    records_start : int
+        File location where the TTTR information starts.
     """
 
     def decode(s):
@@ -51,34 +53,34 @@ def read_header_ptu(path):
             tag_name = tag_id + str(tag_idx) if tag_idx > -1 else tag_id
 
             if tag_type_code in (HeaderTypes.empty8, HeaderTypes.int8, HeaderTypes.bit_set64, HeaderTypes.color8):
-                tag_int = struct.unpack("<q", s[40:])[0]
+                tag_int = struct.unpack('<q', s[40:])[0]
                 header[tag_name] = tag_int
 
             elif tag_type_code == HeaderTypes.bool8:
-                tag_int = struct.unpack("<q", s[40:])[0]
+                tag_int = struct.unpack('<q', s[40:])[0]
                 header[tag_name] = bool(tag_int)
 
             elif tag_type_code == HeaderTypes.float8:
-                tag_int = struct.unpack("<d", s[40:])[0]
+                tag_int = struct.unpack('<d', s[40:])[0]
                 header[tag_name] = tag_int
 
             elif tag_type_code == HeaderTypes.float8_array:
-                tag_int = struct.unpack("<q", s[40:])[0]
+                tag_int = struct.unpack('<q', s[40:])[0]
                 ss = file.read(tag_int)
-                header[tag_name] = struct.unpack("<" + (tag_int / 8) * "d", ss)
+                header[tag_name] = struct.unpack('<' + (tag_int / 8) * 'd', ss)
 
             elif tag_type_code == HeaderTypes.datetime:
-                tag_int = struct.unpack("<d", s[40:])[0]
+                tag_int = struct.unpack('<d', s[40:])[0]
                 header[tag_name] = tag_int
 
             elif tag_type_code in (HeaderTypes.ANSI_string, HeaderTypes.wide_string):
-                tag_int = struct.unpack("<q", s[40:])[0]
+                tag_int = struct.unpack('<q', s[40:])[0]
                 ss = file.read(tag_int)
-                if tag_name in ("$Comment", "File_Comment"): continue
+                if tag_name in ('$Comment', 'File_Comment'): continue
                 header[tag_name] = decode(ss)
 
             elif tag_type_code == HeaderTypes.binary_blob:
-                tag_int = struct.unpack("<q", s[40:])[0]
+                tag_int = struct.unpack('<q', s[40:])[0]
                 ss = file.read(tag_int)
                 header[tag_name] = ss
             else:
@@ -91,115 +93,114 @@ def read_header_ptu(path):
     return header, records_start
 
 
-def read_header_pt3(fname):
+def read_header_pt3(path):
     """
-    load the header file of a .pt3 file
-    
+    Read header of a .pt3 file.
+
     Parameters
     ----------
-    
-    fname: full path to .pt3 file
-    
-    
+    path : string or pathlib.Path
+        The file to read.
+
     Returns
-    --------
-    
-    header: header of the pt3 file
-    
-    rec_start: file location where the TTTR information starts    
+    -------
+    header : dict
+        The header of the pt3 file.
+    records_start : int
+        File location where the TTTR information starts.
     """
 
-    f = open(fname, "rb")
+    def decode(s):
+        return s.decode('utf-8').rstrip('\x00')
 
-    # ASCII header
-    s = f.read(328)
-    header = dict()
-    header["Ident"] = s[:16].decode("utf-8").rstrip("\x00")
-    header["FormatVersion"] = s[16:22].decode("utf-8").rstrip("\x00")
-    header["CreatorName"] = s[22:40].decode("utf-8").rstrip("\x00")
-    header["CreatorVersion"] = s[40:52].decode("utf-8").rstrip("\x00")
-    header["FileTime"] = s[52:70].decode("utf-8").rstrip("\x00")
-    header["CRFL"] = s[70:72].decode("utf-8").rstrip("\x00")
-    header["CommentField"] = s[72:].decode("utf-8").rstrip("\x00")
+    with open(path, 'rb') as file:
+        # ASCII header
+        s = file.read(328)
+        header = {'Ident': decode(s[:16]),
+                  'FormatVersion': decode(s[16:22]),
+                  'CreatorName': decode(s[22:40]),
+                  'CreatorVersion': decode(s[40:52]),
+                  'FileTime': decode(s[52:70]),
+                  'CRFL': decode(s[70:72]),
+                  'CommentField': decode(s[72:])}
 
-    # binary header
-    DISPCURVES = 8
-    s = f.read(72)
-    h = ["Curves", "BitsPerRecord", "RoutingChannels", "NumberOfBoards",
-         "ActiveCurve", "MeasMode", "SubMode", "RangeNo", "Offset", "Tacq",
-         "StopAt", "StopOnOvfl", "Restart", "DispLinLog", "DispTimeFrom",
-         "DispTimeTo", "DispCountsFrom", "DispCountsTo"]
-    # Tacq in ms
-    # DispTime in ns
-    for i, val in enumerate(struct.unpack("<" + 18 * "I", s)):
-        header[h[i]] = val
+        # binary header
+        DISPCURVES = 8
+        s = file.read(72)
+        # Tacq in ms
+        # DispTime in ns
+        for key, value in zip(('Curves', 'BitsPerRecord', 'RoutingChannels', 'NumberOfBoards',
+                               'ActiveCurve', 'MeasMode', 'SubMode', 'RangeNo', 'Offset', 'Tacq',
+                               'StopAt', 'StopOnOvfl', 'Restart', 'DispLinLog', 'DispTimeFrom',
+                               'DispTimeTo', 'DispCountsFrom', 'DispCountsTo'),
+                              struct.unpack('<' + 18 * 'I', s)):
+            header[key] = value
 
-    s = f.read(DISPCURVES * 8)
-    header["DispCurves"] = np.array(struct.unpack("<" + DISPCURVES * "II", s)).reshape(2, DISPCURVES)
-    s = f.read(12 * 3)
-    header["Params"] = np.array(struct.unpack("<fffffffff", s)).reshape(3, 3)
-    s = f.read(36)
-    h = ["RepeatMode", "RepeatsPerCurve", "RepeatTime", "RepeatWaitTime"]
-    for i, val in enumerate(struct.unpack("<" + 4 * "I", s[:16])):
-        header[h[i]] = val
-    header["ScriptName"] = s[16:].decode("utf-8").rstrip("\x00")
+        s = file.read(DISPCURVES * 8)
+        header['DispCurves'] = np.array(struct.unpack('<' + DISPCURVES * 'II', s)).reshape(2, DISPCURVES)
+        s = file.read(12 * 3)
+        header['Params'] = np.array(struct.unpack('<fffffffff', s)).reshape(3, 3)
+        s = file.read(36)
+        for key, value in zip(('RepeatMode', 'RepeatsPerCurve', 'RepeatTime', 'RepeatWaitTime'),
+                              struct.unpack('<' + 4 * 'I', s[:16])):
+            header[key] = value
+        header['ScriptName'] = decode(s[16:])
 
-    # board specific header
-    s = f.read(24)
-    header["HardwareIdent"] = s[:16].decode("utf-8").rstrip("\x00")
-    header["HardwareVersion"] = s[16:].decode("utf-8").rstrip("\x00")
-    h = ["HardwareSerial", "SyncDivider", "CFDZeroCross0", "CFDLevel0",
-         "CFDZeroCross1", "CFDLevel1", "Resolution", "RouterModelCode",
-         "RouterEnabled", "RtChan1_InputType", "RtChan1_InputLevel",
-         "RtChan1_InputEdge", "RtChan1_CFDPresent", "RtChan1_CFDLevel",
-         "RtChan1_CFDZeroCross", "RtChan2_InputType", "RtChan2_InputLevel",
-         "RtChan2_InputEdge", "RtChan2_CFDPresent", "RtChan2_CFDLevel",
-         "RtChan2_CFDZeroCross", "RtChan3_InputType", " RtChan3_InputLevel",
-         "RtChan3_InputEdge", "RtChan3_CFDPresent", "RtChan3_CFDLevel",
-         "RtChan3_CFDZeroCross", "RtChan4_InputType", " RtChan4_InputLevel",
-         "RtChan4_InputEdge", "RtChan4_CFDPresent", "RtChan4_CFDLevel",
-         "RtChan4_CFDZeroCross"]
+        # board specific header
+        s = file.read(24)
+        header['HardwareIdent'] = decode(s[:16])
+        header['HardwareVersion'] = decode(s[16:])
 
-    s = f.read(33 * 4)
-    for i, val in enumerate(struct.unpack("<" + 6 * "I" + "f" + 26 * "I", s)):
-        header[h[i]] = val
+        s = file.read(33 * 4)
+        for key, value in zip(('HardwareSerial', 'SyncDivider', 'CFDZeroCross0', 'CFDLevel0',
+                               'CFDZeroCross1', 'CFDLevel1', 'Resolution', 'RouterModelCode',
+                               'RouterEnabled', 'RtChan1_InputType', 'RtChan1_InputLevel',
+                               'RtChan1_InputEdge', 'RtChan1_CFDPresent', 'RtChan1_CFDLevel',
+                               'RtChan1_CFDZeroCross', 'RtChan2_InputType', 'RtChan2_InputLevel',
+                               'RtChan2_InputEdge', 'RtChan2_CFDPresent', 'RtChan2_CFDLevel',
+                               'RtChan2_CFDZeroCross', 'RtChan3_InputType', ' RtChan3_InputLevel',
+                               'RtChan3_InputEdge', 'RtChan3_CFDPresent', 'RtChan3_CFDLevel',
+                               'RtChan3_CFDZeroCross', 'RtChan4_InputType', ' RtChan4_InputLevel',
+                               'RtChan4_InputEdge', 'RtChan4_CFDPresent', 'RtChan4_CFDLevel',
+                               'RtChan4_CFDZeroCross'),
+                              struct.unpack('<' + 6 * 'I' + 'f' + 26 * 'I', s)):
+            header[key] = value
 
         # TTTR mode specific header
-    h = ["ExtDevices", "Reserved1", "Reserved2", "CntRate0", "CntRate1",
-         "StopAfter", "StopReason", "Records", "SpecHeaderLength"]
-    s = f.read(9 * 4)
-    for i, val in enumerate(struct.unpack("<" + 9 * "I", s)):
-        header[h[i]] = val
+        s = file.read(9 * 4)
+        for key, value in zip(('ExtDevices', 'Reserved1', 'Reserved2', 'CntRate0', 'CntRate1',
+                               'StopAfter', 'StopReason', 'Records', 'SpecHeaderLength'),
+                              struct.unpack('<' + 9 * 'I', s)):
+            header[key] = value
 
         # Imaging Header
+        s = file.read(8)
+        header['dimensions'], header['Ident'] = struct.unpack('<II', s)
 
-    s = f.read(8)
-    header["dimensions"], header["Ident"] = struct.unpack("<II", s)
+        if header['Ident'] == 1:
+            header['ScannerType'] = 'PI E710'
+            h = ('TimerPerPixel', 'Acceleration', 'Pattern',
+                 'Reserved', 'X0', 'Y0', 'PixX', 'PixY', 'PixResol', 'TStartTo',
+                 'TStopTo', 'TStartFro', 'TStopFro')
+            s = file.read(13 * 4)
 
-    if header["Ident"] == 1:
-        header["ScannerType"] = "PI E710"
-        h = ["TimerPerPixel", "Acceleration", "Pattern",
-             "Reserved", "X0", "Y0", "PixX", "PixY", "PixResol", "TStartTo",
-             "TStopTo", "TStartFro", "TStopFro"]
-        s = f.read(13 * 4)
+            for i, value in enumerate(struct.unpack('<IIIIffIIfffff', s)):
+                header[h[i]] = value
 
-        for i, val in enumerate(struct.unpack("<IIIIffIIfffff", s)):
-            header[h[i]] = val
+        if header['Ident'] == 4:
+            header['ScannerType'] = 'KDT180-100-lm'
+            h = ('Velocity', 'Acceleration', 'Pattern', 'Reserved', 'X0', 'Y0',
+                 'PixX', 'PixY', 'PixResol')
+            s = file.read(9 * 4)
+            for i, value in enumerate(struct.unpack('<IIIIIIffIIf', s)):
+                header[h[i]] = value
 
-    if header["Ident"] == 4:
-        header["ScannerType"] = "KDT180-100-lm"
-        h = ["Velocity", "Acceleration", "Pattern", "Reserved", "X0", "Y0",
-             "PixX", "PixY", "PixResol"]
-        s = f.read(9 * 4)
-        for i, val in enumerate(struct.unpack("<IIIIIIffIIf", s)):
-            header[h[i]] = val
+        if header['Ident'] == 3:
+            header['ScannerType'] = 'LSM'
+            h = ('Frame', 'LineStart', 'LineStop', 'Pattern', 'PixX', 'PixY')
+            s = file.read(6 * 4)
+            for i, value in enumerate(struct.unpack('<IIIIII', s)):
+                header[h[i]] = value
 
-    if header["Ident"] == 3:
-        header["ScannerType"] = "LSM"
-        h = ["Frame", "LineStart", "LineStop", "Pattern", "PixX", "PixY"]
-        s = f.read(6 * 4)
-        for i, val in enumerate(struct.unpack("<IIIIII", s)):
-            header[h[i]] = val
-
-    rec_start = f.tell()
-    return header, rec_start
+        records_start = file.tell()
+    return header, records_start
--- pyflim/tests/io/picoquant.py
+++ pyflim/tests/io/picoquant.py
@@ -1,16 +1,30 @@
 import json
 import unittest
 
-from pyflim.io.pq_header import read_header_ptu
+import numpy as np
+
+from pyflim.io.pq_header import read_header_ptu, read_header_pt3
 
 
 class TestHeader(unittest.TestCase):
 
     def test_read_ptu_header(self):
-        with open('tests/io/example.json') as file:
+        with open('tests/io/ptu_example.json') as file:
             expected_header, expected_records_start = json.load(file)
 
-        header, records_start = read_header_ptu('tests/io/example.ptu')
+        header, records_start = read_header_ptu('tests/io/ptu_example.ptu')
+
+        self.assertDictEqual(header, expected_header)
+        self.assertEqual(records_start, expected_records_start)
+
+    def test_read_pt3_header(self):
+        with open('tests/io/pt3_example.json') as file:
+            expected_header, expected_records_start = json.load(file)
+
+        header, records_start = read_header_pt3('tests/io/pt3_example.pt3')
+        for key, value in header.items():
+            if isinstance(value, np.ndarray):
+                header[key] = value.tolist()
 
         self.assertDictEqual(header, expected_header)
         self.assertEqual(records_start, expected_records_start)
