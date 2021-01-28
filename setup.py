c9234a902f9941766c9c09f431d1807a30d5161b
Mauro Silberberg <maurosilber@gmail.com>
2021-01-28 18:27:19 -0300

    Updated setup

--- setup.py
+++ setup.py
@@ -1,12 +1,5 @@
-from setuptools import setup, find_packages
+#!/usr/bin/env python3
+from setuptools import setup
 
-setup(
-    name='pyflim',
-    version='0.0.1',
-    packages=find_packages(),
-    url='https://github.com/maurosilber/pyflim',
-    license='MIT',
-    author='Mauro Silberberg',
-    author_email='maurosilber@gmail.com',
-    install_requires=['numpy', 'numba', 'binlets']
-)
+if __name__ == "__main__":
+    setup()
